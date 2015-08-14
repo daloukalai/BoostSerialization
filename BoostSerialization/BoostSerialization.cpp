d0664e57f960408e60b2d2057f2a21d7be8ea7a1
Dalou kalaivendhan <daloukalai@gmail.com>
2015-08-14 11:02:16 +0530

    3rd commit

--- BoostSerialization/BoostSerialization.cpp
+++ BoostSerialization/BoostSerialization.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<assert.h>
 #include<fstream>
 #include<boost/archive/text_iarchive.hpp>
 #include<boost/archive/text_oarchive.hpp>
@@ -38,21 +39,41 @@ private:
 void main()
 {
     cout << "Boost Serialization" << endl;
-    const char* fileName = "C:\\saved.txt";
+    const char* fileName = "C:\\BOOST_SERIALIZED_OBJ.txt";
 
-      // Create some objects
-      const Obj o1(-2, false);
-      const Obj o2;
-      const Obj o3(21, true);
-      const Obj* const p1 = &o1;
+    // Create some objects
+    const Obj o1(-2, false);
+    const Obj o2;
+    const Obj o3(21, true);
+    const Obj* const p1 = &o1;
 
-      // Save data
-      {
+    // Save data
+    {
         // Create an output archive
         ofstream ofs(fileName);
         boost::archive::text_oarchive ar(ofs);
 
-       // Write data
+        // Write data
         ar & o1 & o2 & o3 & p1;
-      }
+    }
+
+    // Restore data
+    Obj restored_o1;
+    Obj restored_o2;
+    Obj restored_o3;
+    Obj* restored_p1;
+    {
+        // Create and input archive
+        std::ifstream ifs(fileName);
+        boost::archive::text_iarchive ar(ifs);
+
+        // Load data
+        ar & restored_o1 & restored_o2 & restored_o3 & restored_p1;
+    }
+    // Make sure we restored the data exactly as it was saved
+    assert(restored_o1 == o1);
+    assert(restored_o2 == o2);
+    assert(restored_o3 == o3);
+    assert(restored_p1 != p1);
+    assert(restored_p1 == &restored_o1);
 }
